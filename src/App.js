221770522991f0d334376cf58d8e2377f187a586
Arthy Umapathy <arthtuma@amazon.com>
2024-12-13 16:39:44 -0500

    amend commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/Theme';
 import GlobalStyles from './styles/GlobalStyles';
 import WorkExperience from './components/WorkExperience';
+import Header from './components/Header';
+import Details from './components/Details';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Header />
-      <Hero />
+      <Details />
       <About />
       <WorkExperience />
       <Projects />
@@ -20,5 +27,6 @@ function App() {
     </ThemeProvider>
   );
 }
+
 
 export default App;
